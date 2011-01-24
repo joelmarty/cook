c3a2ef7da4e98e7ddd1627ea734c03c28d18a529
Alexis Metaireau <alexis@notmyidea.org>
2011-01-24 21:08:29 +0000

    User registration.

--- app/models/User.java
+++ app/models/User.java
@@ -1,12 +1,38 @@
 package models;
 
 import play.db.jpa.*;
+import play.Logger;
 
 import java.util.*;
 import javax.persistence.*;
+import java.security.*;
 
 @Entity
 public class User extends Model{
     public String name;
+    public String email;
     public byte[] password;
+
+    /**
+     * Create an user given the provided informations.
+     *
+     * The password must be the uncyphered, and will be cyphered by this
+     * method.
+     */
+    public static User Create(String name, String email, String password){
+        User user = new User();
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+
+            user.name = name;
+            user.email = email;
+            user.password = md5.digest(password.getBytes("UTF-8"));
+            user.save();
+
+        } catch (Exception e) {
+            Logger.error(e.getMessage());
+        }
+
+        return user;
+    }
 }
