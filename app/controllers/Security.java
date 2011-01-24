b8ce5fd6c583430a0419590428bdad35debc3d94
Joel Marty <10093745@brookes.ac.uk>
2011-01-24 17:54:38 +0000

    added Security.java needed exception catches and stuff

--- app/controllers/Security.java
+++ app/controllers/Security.java
@@ -1,18 +1,31 @@
 package controllers;
 
 import models.User;
-import java.security.MessageDigest;
+import java.security.*;
+import java.io.UnsupportedEncodingException;
+import play.Logger;
 
 public class Security extends Secure.Security {
 
     static boolean authenticate(String username, String password) {
-        MessageDigest md5 = MessageDigest.getInstance("MD5");
-        md5.reset;
-        byte[] hash = md5.digest(password.getBytes("UTF-8"));
+        MessageDigest md5 = null;
+        byte[] hash = null;
+
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+            md5.reset();
+            hash = md5.digest(password.getBytes("UTF-8"));
+        } catch (NoSuchAlgorithmException e) {
+            Logger.error(e, "Oops!");
+        } catch (UnsupportedEncodingException e) {
+            Logger.error(e, "Oops!");
+        }
+
         
         User user = User.find("byName", username).first();
 
         return user != null && user.password.equals(hash);
+        
     }
 
 }
